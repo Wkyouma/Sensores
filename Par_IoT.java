59d08185db84b5b5f25428079e85c5f382b18ea0
Igor terplak Gutierrez <135295319+Wkyouma@users.noreply.github.com>
2024-08-04 22:09:48 -0300

    Add files via upload

---
+++ Par_IoT.java
@@ -0,0 +1,11 @@
+public class Par_IoT {
+    String ID;
+    Sensor sensor;
+    Atuador atuador;
+
+    public Par_IoT(String ID, Sensor sensor, Atuador atuador) {
+        this.ID = ID;
+        this.sensor = sensor;
+        this.atuador = atuador;
+    }
+}
---
+++ SensorFumaca.java
@@ -0,0 +1,13 @@
+public abstract class SensorFumaca implements Sensor {
+
+    int Contador;
+
+    SensorFumaca(int contador) {
+
+        Contador = contador;
+
+    }
+
+    public abstract double ler_dado();
+
+}
